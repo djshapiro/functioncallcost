5e648454571c4986a053f68309f65043ee928ee1
Dan Shapiro <dshapiro@express.com>
2019-04-03 10:07:20 -0400

    deepFreeze

--- index.js
+++ index.js
@@ -1,28 +1,42 @@
 const timeit = require('timeit');
 const get = require('lodash.get');
+const deepFreeze = require('deep-freeze');
 
-const iterations = 500000;
+const iterations = 50000;
 
 const mainGuy = {
-  a: 'yes'
+  a: 'yes',
+  b: {
+    c: 1,
+    d: {
+      e: 'cold',
+      f: {
+        g: [
+          {
+            h: 'v cold',
+          },
+        ],
+      },
+    },
+  },
 };
 
 //This guy doesn't call a function
-const nofunc = (done) => {
+const nofreeze = (done) => {
   let result;
-  result = (mainGuy && mainGuy.a) || '';
+  result = mainGuy;
   done();
 }
 
 //This guy calls a function
-const func = (done) => {
+const freeze = (done) => {
   let result;
-  result = get(mainGuy, 'a', '');
+  result = deepFreeze(mainGuy);
   done();
 }
 
-timeit.howlong(iterations, [func, nofunc], (err, results) => {
+timeit.howlong(iterations, [freeze, nofreeze], (err, results) => {
   console.log('Baseline', results[0].average_step_runtime);
-  console.log('with function invokation', results[1].average_step_runtime);
-  console.log('without function invokation', results[2].average_step_runtime);
+  console.log('with freeze', results[1].average_step_runtime);
+  console.log('without freeze', results[2].average_step_runtime);
 });
